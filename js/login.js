71279f3c69752a0c13d97199ba9d777628c21e23
JoelTanto <mdkjoel@hotmail.com>
2023-02-17 01:45:13 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+/*FORMULARIO*/
 const   $btnSignIn= document.querySelector('.sign-in-btn'),
         $btnSignUp= document.querySelector('.sign-up-btn'),
         $signUp= document.querySelector('.sign-up'),
@@ -21,3 +22,32 @@ function loguear()
         alert("Datos incorrectos")
     }
 }
+/*FIN FORMULARIO
+
+/*RELOJ*/
+const $tiempo = document.querySelector('.tiempo'),
+        $fecha = document.querySelector('.fecha');
+
+        function digitalClock(){
+            let f = new Date(),
+                dia = f.getDate(),
+                mes = f.getMonth() + 1,
+                anio = f.getFullYear(),
+                diaSemana = f.getDay();
+
+                dia = ('0' + dia).slice(-2);
+                mes = ('0' + mes).slice(-2);
+
+
+
+            let timeString = f.toLocaleTimeString();
+                $tiempo.innerHTML = timeString;
+
+            let semana = [ 'DOM', 'LUN', 'MAR', 'MIE', 'JUE', 'VIE', 'SAB'];
+            let showSemana = (semana[diaSemana]);
+                $fecha.innerHTML = `${anio}-${mes}-${dia} ${showSemana}`;
+}
+setInterval(() => {
+    digitalClock()
+}, 1000);
+/*FIN RELOJ*/
